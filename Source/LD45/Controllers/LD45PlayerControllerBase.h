df154dd8ec2a706faec2276fb9171fc737444475
Flubz <FarjadM96@gmail.com>
2019-10-05 15:12:45 +0100

    Item Data System Added.

--- Source/LD45/Controllers/LD45PlayerControllerBase.h
+++ Source/LD45/Controllers/LD45PlayerControllerBase.h
@@ -2,64 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
-#include <Engine/DataTable.h>
-#include <Image.h>
-#include <Engine/StaticMesh.h>
 #include "LD45PlayerControllerBase.generated.h"
-
-UENUM(BlueprintType)
-enum class FItemLocation : uint8
-{
-	Garage UMETA(DisplayName = "Garage"),
-	Bathroom UMETA(DisplayName = "Bathroom"),
-	Kitchen UMETA(DisplayName = "Kitchen"),
-};
-
-USTRUCT(BlueprintType)
-struct FItemEdibilityType : public FTableRowBase
-{
-	GENERATED_BODY()
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FString EdibilityType;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	int32 EdibilityDamage;
-};
-
-USTRUCT(BlueprintType)
-struct FItem : public FTableRowBase
-{
-	GENERATED_BODY()
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FString ItemName;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FString ItemDescription;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	int32 ItemCost;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FItemEdibilityType ItemEdibilityType;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 ItemCount;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	UStaticMesh* ItemMesh;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	UImage* ItemIcon;
-};
-
-USTRUCT(BlueprintType)
-struct FInventory : public FTableRowBase
-{
-	GENERATED_BODY()
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TArray<FItem> Items;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FItemLocation ItemLocation;
-};
 
 UCLASS()
 class LD45_API ALD45PlayerControllerBase : public APlayerController
