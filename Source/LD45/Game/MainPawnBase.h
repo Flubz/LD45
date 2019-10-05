4cdc0abccfb3855f9171d78a353cfd5ca2364057
Flubz <FarjadM96@gmail.com>
2019-10-05 13:21:38 +0100

    Added base classes. Added main map. Added basic Item Data Table.

---
+++ Source/LD45/Game/MainPawnBase.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MainPawnBase.generated.h"
+
+UCLASS()
+class LD45_API AMainPawnBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	AMainPawnBase();
+};
